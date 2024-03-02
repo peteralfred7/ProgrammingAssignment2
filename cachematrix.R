a912e7de575ee3bdc8acfbed014046728dd2930f
peteralfred7 <161953750+peteralfred7@users.noreply.github.com>
2024-03-02 23:54:18 +0800

    Update cachematrix.R
    Assignment from Week 3 of Courser Course: R Programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment # 2
+## by Peter Alfred Martinez
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## initialize to NULL
+  inv <- NULL
+  
+  ## set what would happen to the initial cached matrix created and the changes made to the cached matrix.
+  set <- function (y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  ## get the value of the matrix
+  get <- function() {
+    x
+  }
+  
+  
+  ## get the inverse using solve function
+  setinverse <- function(solve) {
+    inv <<- solve
+  }
+  
+  ## get the inverse matrix
+  getinverse <- function() {
+    inv
+  }
+  
+  ## passes the value of the function makeCacheMatrix
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  # if the inverse exists, get it.
+  if( !is.null(inv) ) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  #if the inverse does no exist, compute for the inverse
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
 }
+
+
+#Testing the code
+# generating a random 3x3 matrix
+ms <-matrix(rpois(9,3),nrow=3)
+ms
+
+#Actual testing 
+m1 <- makeCacheMatrix(ms)
+m1
+cacheSolve(m1)
